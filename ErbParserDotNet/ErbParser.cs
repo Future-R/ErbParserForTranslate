c370fe65abd020f449d01daa4b06f6de17c9288c
Future-R <future.r@foxmail.com>
2025-07-20 15:02:12 +0800

    fix: DIM有时漏提取

--- ErbParserDotNet/ErbParser.cs
+++ ErbParserDotNet/ErbParser.cs
@@ -264,7 +264,7 @@ public class ERBParser
                     string rightValue = lineString.Substring(eqIndex + 1).Trim();
 
                     var leftEnumer = leftValue.Split(' ')
-                        .Where(arg => !string.IsNullOrWhiteSpace(arg) && arg != "<Tab>");
+                        .Where(arg => !string.IsNullOrWhiteSpace(arg.Replace("<Tab>", "")));
                     varNameList.Add(leftEnumer.LastOrDefault(), contexts);
 
                     // 类型推导，如果是数值型，那么右值不用翻译，continue掉
